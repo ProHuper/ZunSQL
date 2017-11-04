f10ade45ed3b8dedcf4708f05578bbbcbab8113a
ProHuper <871027967@qq.com>
2017-11-04 22:54:09 +0800

    completed the general framework and added some api from the below module.

---
+++ src/main/java/npu/zunsql/ve/Column.java
@@ -0,0 +1,13 @@
+package npu.zunsql.ve;
+
+class Column
+{
+	String ColumnName;
+	String ColumnType;
+	
+	public Column(String pName,String pType)
+	{
+		ColumnName=pName;
+		ColumnType=pType;
+	}
+}
