78f2ed778c0091f7ddc7f48e414b24791d9c59b4
Cholerae Hu <choleraehyq@gmail.com>
2017-11-07 10:45:09 +0800

    codegen: add Insert/Create/Delete/Drop support.
    Only missing statement is update.
    
    Signed-off-by: Cholerae Hu <choleraehyq@gmail.com>

--- src/main/java/npu/zunsql/codegen/CodeGenerator.java
+++ src/main/java/npu/zunsql/codegen/CodeGenerator.java
@@ -29,6 +29,7 @@ public class CodeGenerator {
             }
             if (!inTransaction) {
                 ret.add(new Instruction(OpCode.Transaction, null, null, null));
+            }
                 TYPE_SWITCH:
                 {
                     if (statement instanceof Select) {
@@ -45,23 +46,98 @@ public class CodeGenerator {
                             }
                             // Do not support other expression now due to lack of OpCode type.
                         }
-                        Expression where = ((Select)statement).where;
-                        if (where != null) {
-                            ret.add(new Instruction(OpCode.BeginFilter, null, null, null));
-                            if (where instanceof BinaryExpression) {
-                                BinaryExpression bin = (BinaryExpression)where;
-                                ret.addAll(BinaryExpressionToFilter(bin));
-                            }
-                            ret.add(new Instruction(OpCode.EndFilter, null, null, null));
-                            // Do not support other expression now due to lack of OpCode type.
-                        }
                         ret.add(new Instruction(OpCode.EndColSelect, null, null, null));
+                        Expression where = ((Select)statement).where;
+                        ret.addAll(WhereToInstruction(where));
                         break TYPE_SWITCH;
+                    }
+                    if (statement instanceof Insert) {
+                        Insert insert = (Insert)statement;
+                        String table = insert.table.tableName.names.get(0);
+                        ret.add(new Instruction(OpCode.Insert, null, null, table));
+                        ret.add(new Instruction(OpCode.BeginItem, null, null, null));
+                        for (int i = 0; i < insert.names.size(); i++) {
+                            String name = ((QualifiedNameExpression)insert.names.get(0)).qname.names.get(0);
+                            String type = null;
+                            String value = insert.expressions.get(0).toString();
+                            if (insert.expressions.get(0) instanceof StringExpression) {
+                                type = "String";
+                            } else if (insert.expressions.get(0) instanceof NumberExpression) {
+                                if (value.contains(".")) {
+                                    type = "Float";
+                                } else {
+                                    type = "Integer";
+                                }
+                            }
+                            ret.add(new Instruction(OpCode.AddItemCol, name, type, value));
+                        }
+                        ret.add(new Instruction(OpCode.EndItem, null, null, null));
+                        break TYPE_SWITCH;
+                    }
+                    if (statement instanceof Create) {
+                        Create create = (Create)statement;
+                        String table = create.table.tableName.names.get(0);
+                        ret.add(new Instruction(OpCode.CreateTable, null, null, table));
+                        for (Column col: create.columns) {
+                            String name = ((QualifiedNameExpression)col.name).qname.names.get(0);
+                            String type = DataTypeToString(col.type);
+                            ret.add(new Instruction(OpCode.AddCol, name, type, null));
+                        }
+                        ret.add(new Instruction(OpCode.BeginPK, null, null, null));
+                        for (Column col: create.columns) {
+                            if (col.isPrimaryKey) {
+                                String name = ((QualifiedNameExpression)col.name).qname.names.get(0);
+                                ret.add(new Instruction(OpCode.AddPK, name, null, null));
+                            }
+                        }
+                        ret.add(new Instruction(OpCode.EndPK, null, null, null));
+                        break TYPE_SWITCH;
+                    }
+                    if (statement instanceof Delete) {
+                        Delete delete = (Delete)statement;
+                        String table = delete.from.tableName.names.get(0);
+                        ret.add(new Instruction(OpCode.Delete, null, null, table));
+                        Expression where = delete.where;
+                        ret.addAll(WhereToInstruction(where));
+                        break TYPE_SWITCH;
+                    }
+                    if (statement instanceof Drop) {
+                        Drop drop = (Drop)statement;
+                        String table = drop.table.tableName.names.get(0);
+                        ret.add(new Instruction(OpCode.DropTable, null, null, table));
+                        break TYPE_SWITCH;
+                    }
+                    if (statement instanceof Update) {
+                        Update update = (Update)statement;
+                        String table = update.table.tableName.names.get(0);
+                        ret.add(new Instruction(OpCode.Update, null, null, table));
+
+                        Expression where = update.where;
+                        ret.addAll(WhereToInstruction(where));
+                        for (Assignment a: update.updates) {
+                            String name = a.name.names.get(0);
+                            // TODO(Cholerae): set translation
+                        }
                     }
                 }
                 ret.add(new Instruction(OpCode.Execute, null, null, null));
+            if (!inTransaction) {
                 ret.add(new Instruction(OpCode.Commit, null, null, null));
             }
+        }
+        return ret;
+    }
+
+    private static List<Instruction> WhereToInstruction(Expression where) {
+        List<Instruction> ret = new ArrayList<>();
+        if (where != null) {
+            ret.add(new Instruction(OpCode.BeginFilter, null, null, null));
+            if (where instanceof BinaryExpression) {
+                BinaryExpression bin = (BinaryExpression)where;
+                ret.addAll(BinaryExpressionToFilter(bin));
+            }
+            ret.add(new Instruction(OpCode.EndFilter, null, null, null));
+            // Do not support other expression now due to lack of OpCode type.
         }
         return ret;
     }
@@ -102,6 +178,21 @@ public class CodeGenerator {
                 break;
             case EQ:
                 ret = "=";
+        }
+        return ret;
+    }
+
+    private static String DataTypeToString(DataType dt) {
+        String ret = null;
+        switch (dt) {
+            case INT:
+                ret = "Integer";
+                break;
+            case DOUBLE:
+                ret = "Float";
+                break;
+            case VARCHAR:
+                ret = "String";
         }
         return ret;
     }
--- src/test/java/npu/zunsql/codegen/CodeGeneratorTest.java
+++ src/test/java/npu/zunsql/codegen/CodeGeneratorTest.java
@@ -67,4 +67,68 @@ public class CodeGeneratorTest {
                         new Instruction(OpCode.Commit, null, null, null)
                 ));
     }
+
+    @Test
+    public void testGenerateByteCodeDelete() {
+        assertEquals(
+                CodeGenerator.GenerateByteCode(Arrays.asList(Parser.parse("delete from table_1 where x=10 and y=100"))),
+                Arrays.asList(
+                        new Instruction(OpCode.Transaction, null, null, null),
+                        new Instruction(OpCode.Delete, null, null, "table_1"),
+                        new Instruction(OpCode.BeginFilter, null, null, null),
+                        new Instruction(OpCode.Filter, "x", "=", "10"),
+                        new Instruction(OpCode.And, null, null, null),
+                        new Instruction(OpCode.Filter, "y", "=", "100"),
+                        new Instruction(OpCode.EndFilter, null, null, null),
+                        new Instruction(OpCode.Execute, null, null, null),
+                        new Instruction(OpCode.Commit, null, null, null)
+                ));
+    }
+
+    @Test
+    public void testGenerateByteCodeInsert() {
+        assertEquals(
+                CodeGenerator.GenerateByteCode(Arrays.asList(Parser.parse("insert into table_1(x,y,z) values(1,2.2,'ccc')"))),
+                Arrays.asList(
+                        new Instruction(OpCode.Transaction, null, null, null),
+                        new Instruction(OpCode.Insert, null, null, "table_1"),
+                        new Instruction(OpCode.BeginItem, null, null, null),
+                        new Instruction(OpCode.AddItemCol, "x", "Integer", "1"),
+                        new Instruction(OpCode.AddItemCol, "y", "Float", "1"),
+                        new Instruction(OpCode.AddItemCol, "z", "String", "cc"),
+                        new Instruction(OpCode.EndItem, null, null, null),
+                        new Instruction(OpCode.Execute, null, null, null),
+                        new Instruction(OpCode.Commit, null, null, null)
+                ));
+    }
+
+    @Test
+    public void testGenerateByteCodeCreate() {
+        assertEquals(
+                CodeGenerator.GenerateByteCode(Arrays.asList(Parser.parse("create table test ( x int primary key, y double, z varchar) "))),
+                Arrays.asList(
+                        new Instruction(OpCode.Transaction, null, null, null),
+                        new Instruction(OpCode.CreateTable, null, null, "test"),
+                        new Instruction(OpCode.AddCol, "x", "Integer", null),
+                        new Instruction(OpCode.AddCol, "y", "Float", null),
+                        new Instruction(OpCode.AddCol, "z", "String", null),
+                        new Instruction(OpCode.BeginPK, null, null, null),
+                        new Instruction(OpCode.AddPK, "x", null, null),
+                        new Instruction(OpCode.EndPK, null, null, null),
+                        new Instruction(OpCode.Execute, null, null, null),
+                        new Instruction(OpCode.Commit, null, null, null)
+                ));
+    }
+
+    @Test
+    public void testGenerateByteCodeDrop() {
+        assertEquals(
+                CodeGenerator.GenerateByteCode(Arrays.asList(Parser.parse("drop table test  "))),
+                Arrays.asList(
+                        new Instruction(OpCode.Transaction, null, null, null),
+                        new Instruction(OpCode.DropTable, null, null, "test"),
+                        new Instruction(OpCode.Execute, null, null, null),
+                        new Instruction(OpCode.Commit, null, null, null)
+                ));
+    }
 }
