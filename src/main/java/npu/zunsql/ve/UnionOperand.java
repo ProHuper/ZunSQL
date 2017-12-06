fecf62c4e31a669c0ef729e86fcbc28a7f4c7721
ProHuper <871027967@qq.com>
2017-12-06 14:28:30 +0800

    "update"

---
+++ src/main/java/npu/zunsql/ve/UnionOperand.java
@@ -0,0 +1,18 @@
+package npu.zunsql.ve;
+
+import npu.zunsql.tree.BasicType;
+
+public class UnionOperand {
+    private String value;
+    private BasicType type;
+    public UnionOperand(BasicType pType,String pValue){
+        value=pValue;
+        type=pType;
+    }
+    public BasicType getType() {
+        return type;
+    }
+    public String getValue(){
+        return value;
+    }
+}
---
+++ src/test/java/npu/zunsql/ve/ExpressionTest.java
@@ -0,0 +1,13 @@
+package npu.zunsql.ve;
+
+import org.junit.Test;
+
+/**
+ * Created by Huper on 2017/12/6.
+ */
+public class ExpressionTest {
+    @Test
+    void testExpression(){
+
+    }
+}
