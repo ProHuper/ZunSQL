f275eb2ffc805407e1bc2fae4a77cbfb366e0798
pop1h3 <3056097205@qq.com>
2017-12-07 16:08:05 +0800

    一个用例层，打开数据库，执行SQL语句，关闭数据库。

---
+++ src/main/java/npu/zunsql/DBInstance.java
@@ -0,0 +1,47 @@
+package npu.zunsql;
+
+import npu.zunsql.codegen.CodeGenerator;
+import npu.zunsql.sqlparser.Parser;
+import npu.zunsql.sqlparser.ast.Relation;
+import npu.zunsql.ve.QueryResult;
+import npu.zunsql.ve.VirtualMachine;
+import npu.zunsql.tree.Database;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class DBInstance
+{
+	private Database db;
+	
+	public boolean Open(String name)
+	{	
+		db = new Database(name);
+		if(db!=null)return true;
+		  else return false;
+	}
+
+	public QueryResult Execute(String statement)
+	{
+		//定义一个List<Relation>，将parse()返回的Relation对象填入
+		List<Relation> statements = new ArrayList<Relation>();
+		statements.add(Parser.parse(statement));
+
+		//得到查询的结果
+		VirtualMachine vm = new VirtualMachine(db);
+		
+		List<Instruction> array = CodeGenerator.GenerateByteCode(statements);
+		for(Instruction s: array){
+			vm.run(s);
+		}
+		
+		//执行select语句返回结果集，其他语句默认情况下执行成功,结果集为null。
+        return vm.result;
+	}
+	
+	public boolean Close()
+	{
+		if(db!=null&&db.close())return true;
+		else return false;
+	}
+}
